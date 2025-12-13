5ce79a2e40c44c6d09380a55f9467b0e8a37c428
Kaito Sasai <bouzu3.0329@gmail.com>
2025-12-13 20:31:36 +0900

    画像表示できるようにしました

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,15 @@ fn App() -> impl IntoView {
 #[component]
 fn Header() -> impl IntoView {
     view! {
-        <header class="header">
-            <label for="sidemenu" class="menu-btn">"☰"</label>
-            <span class="title">biestar</span>
-            <input type="checkbox" id="sidemenu" hidden />
-            <nav class="sidebar">
-                <a href="#">"ホーム"<br/></a>
-                <a href="#">"投稿"<br/></a>
-                <a href="#">"プロフ"<br/></a>
-            </nav>
-        </header>
+        <header class="header"> 
+            <label for="sidemenu" class="menu-btn">"☰"</label> 
+            <img src="images/tabicon.JPG" alt="アイコン" height="80px"/> 
+        </header> 
+        <input type="checkbox" id="sidemenu" hidden/> 
+        <nav class="sidebar"> 
+            <a href="#">"ホーム"<br/></a>
+            <a href="#">"投稿"<br/></a>
+            <a href="#">"プロフ"<br/></a> 
+        </nav>
     }
 }
