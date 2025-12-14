778a4c089c3055bfa73b7b1f925f37cad8ac9d27
Pironium <94836916+Pironium-dev@users.noreply.github.com>
2025-12-14 22:20:45 +0900

    headerから移行

--- src/app.rs
+++ src/app.rs
@@ -31,6 +31,33 @@ pub fn App() -> impl IntoView {
 
         // sets the document title
         //<Title text="Welcome to Leptos"/>
+        <Header/>
 
+    }
+}
+
+#[component]
+fn Header() -> impl IntoView {
+    view! {
+        <header class="header">
+            <label for="sidemenu" style="margin-left: 10px">
+                <img src="./images/menu_line72.png" alt="メニュー" height="40px"/>
+            </label>
+            <div class="divider"></div>
+            <img src="./images/tabicon.JPG" alt="アイコン" class="logo" height="40px"/>
+            <div class="search-wrap">
+                <img src="./images/search_fill48.png" class="search-icon" />
+                <input type="text" class="searchbar" placeholder="タブ検索"/>
+            </div>
+            <img src="./images/beru.png" alt="アイコン" class="beru" height="40px"/> 
+            <img src="./images/kariicon.jpg" alt="アイコン" class="kariicon" height="40px"/> 
+        </header> 
+        <input type="checkbox" id="sidemenu" hidden/> 
+        <label for="sidemenu" class="overlay"></label>
+        <nav class="sidebar"> 
+            <a>"ホーム"<br/></a>
+            <a>"投稿"<br/></a>
+            <a>"プロフ"<br/></a> 
+        </nav>
     }
 }
