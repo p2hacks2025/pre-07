9f077881e45c62af9f0e329b7c9432a4525bca20
Kaito Sasai <bouzu3.0329@gmail.com>
2025-12-14 12:18:37 +0900

    sidebarの下のバーをつけました

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,17 @@ fn App() -> impl IntoView {
 fn Header() -> impl IntoView {
     view! {
         <header class="header"> 
-            <label for="sidemenu">"☰"</label> 
+            <label for="sidemenu" style="margin-left: 10px">"☰"</label>
+            <div class="divider"></div>
             <img src="./images/tabicon.JPG" alt="アイコン" class="logo" height="40px"/> 
             <img src="./images/beru.png" alt="アイコン" class="beru" height="40px"/> 
             <img src="./images/kariicon.jpg" alt="アイコン" class="kariicon" height="40px"/> 
         </header> 
         <input type="checkbox" id="sidemenu" hidden/> 
         <nav class="sidebar"> 
-            <a href="#">"ホーム"<br/></a>
-            <a href="#">"投稿"<br/></a>
-            <a href="#">"プロフ"<br/></a> 
+            <a>"ホーム"<br/></a>
+            <a>"投稿"<br/></a>
+            <a>"プロフ"<br/></a> 
         </nav>
     }
 }
