f89d9fff565a4c02bc2a57648dd07d9b51a9ff61
kazumasa <highhopes2032@gmail.com>
2025-12-14 18:01:35 +0900

    投稿画面の属性選択ボタン実装とタグスペースのずれ修正

--- src/main.rs
+++ src/main.rs
@@ -39,5 +39,15 @@ fn PostScreen() -> impl IntoView{
                     </div>
                 </div>
         </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
+            <label class="form-check-label" for="flexRadioDefault1">
+                初心者
+            </label>
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+            <label class="form-check-label" for="flexRadioDefault2">
+                上級者
+            </label>
+        </div>
     }
 }
