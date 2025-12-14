9a8df7d33fe248704fcd8a65b59f5debb52a5ea9
kazumasa <highhopes2032@gmail.com>
2025-12-14 15:44:31 +0900

    ポスト時の属性のスイッチを実装、リクのページ待ち

--- src/main.rs
+++ src/main.rs
@@ -36,5 +36,16 @@ fn Header() -> impl IntoView {
             <a>"投稿"<br/></a>
             <a>"プロフ"<br/></a> 
         </nav>
+
+        /*<div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
+            <label class="form-check-label" for="flexRadioDefault1">
+                初心者
+            </label>
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+            <label class="form-check-label" for="flexRadioDefault2">
+                上級者
+            </label>
+        </div>*/
     }
 }
