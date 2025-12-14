7eccf54afad9643d39b97fd88452ddb3c4f53bfc
lemon215 <takiya243@gmail.com>
2025-12-14 16:58:37 +0900

    style.css man.rs images 投稿画面のレイアウトを作成。配置は未定

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,35 @@ fn main() {
 #[component]
 fn App() -> impl IntoView{
     view!{
+        <PostScreen/>
     }
 }
 
 #[component]
 fn PostScreen() -> impl IntoView{
     view!{
-        
+        <div class="flex"></div>
+        <div class="box" id="side-space-left">One</div>
+        <div class="outer">
+                <div class="post-function">
+                    <input class="title-space" type="text" placeholder="タイトルを入力"/> <br/>
+                    <div class="tag-space">
+                        <p>"asfd"</p>
+                    </div>
+                    <div class="text-area-space">
+                        <textarea class="text-space" placeholder="内容を入力"/> <br/>
+                        <div class="text-function">
+                            <button type="button" class="picture">
+                                <img src="../../images/seal_certificate_line72.png"/>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="button-container">
+                        <button type="button" class="post-button">
+                            <img src="../../images/mailing_fill72.png"/>
+                        </button>
+                    </div>
+                </div>
+        </div>
     }
 }
