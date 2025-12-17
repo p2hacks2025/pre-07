368234826e4d77f899386ea8af3159fa5125f3bb
Pironium <94836916+Pironium-dev@users.noreply.github.com>
2025-12-17 13:13:39 +0900

    DBを取得する関数に変更

--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,17 @@
 use leptos::prelude::*;
 #[cfg(feature = "ssr")]
-use {tokio::sync, mongodb::{Client, bson::doc}, std::fs, futures::StreamExt, serde::{Deserialize, Serialize}};
+use {tokio::sync, mongodb::{Client, Database, bson::doc}, std::fs, futures::StreamExt, serde::{Deserialize, Serialize}};
 
 // DBの設定
 #[cfg(feature = "ssr")]
-static DB_CLIENT: sync::OnceCell<Client> = sync::OnceCell::const_new();
+static DB: sync::OnceCell<Database> = sync::OnceCell::const_new();
 
 #[cfg(feature = "ssr")]
-async fn get_db_client() -> Client{
-    DB_CLIENT.get_or_init(|| async {
+async fn get_db_client() -> Database{
+    DB.get_or_init(|| async {
         let path = fs::read_to_string("./passwords/mongoDB_login_url.txt").expect("Couldn't read mongoDB path file");
 
-        Client::with_uri_str(path).await.unwrap()
+        Client::with_uri_str(path).await.unwrap().database("biestar")
 
     }).await.clone()
 }
