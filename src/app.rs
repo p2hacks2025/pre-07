a70cb118f9cd241befd655c5a1891376345409a0
lemon215 <takiya243@gmail.com>
2025-12-18 15:01:21 +0900

    post-screen　上級者から経験者

--- src/app.rs
+++ src/app.rs
@@ -71,11 +71,11 @@ fn PostScreen() -> impl IntoView{
         <div class="form-check">
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"></input>
             <label class="form-check-label" for="flexRadioDefault1">
-                初心者
+                "初心者"
             </label>
             <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
             <label class="form-check-label" for="flexRadioDefault2">
-                上級者
+                "経験者"
             </label>
         </div>
     }
