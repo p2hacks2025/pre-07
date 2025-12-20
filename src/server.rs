3a1da8bc024b751d8f6fb14e2bec7f93db6a4d7a
Pironium <94836916+Pironium-dev@users.noreply.github.com>
2025-12-20 14:45:50 +0900

    (さっきのコミットと説明が逆)タグを検索する機能追加

--- src/server.rs
+++ src/server.rs
@@ -181,7 +181,7 @@ pub async fn search_tag_with_exact(tag: String) -> Result<Option<String>, Server
 #[server]
 pub async fn search_tag_with_prefix(tag: String, amount: i64) -> Result<Vec<String>, ServerFnError>{
     let db_tag = get_db().await.collection::<Tag>("tags");
-    let mut result = db_tag.find(doc!{"tag": {"$regex": format!("^{}", tag)}}).limit(amount).await?;
+    let mut result = db_tag.find(doc!{"tag": {"$regex": format!("^{}", tag), "$options": "i"}}).limit(amount).await?;
     let mut out = vec![];
     while let Some(result) = result.next().await{
         out.push(result.unwrap().tag);
