1b7b2681f8ea1aa2e8dff15e4424f7439bd142c9
Pironium <94836916+Pironium-dev@users.noreply.github.com>
2025-12-20 10:48:58 +0900

    prefixで検索する機能追加

--- src/server.rs
+++ src/server.rs
@@ -177,3 +177,14 @@ pub async fn search_tag_with_exact(tag: String) -> Result<Option<String>, Server
     let result = db_tag.find_one(doc!{"tag": tag}).await?.map(|t| t.tag);
     Ok(result)
 }
+
+#[server]
+pub async fn search_tag_with_prefix(tag: String, amount: i64) -> Result<Vec<String>, ServerFnError>{
+    let db_tag = get_db().await.collection::<Tag>("tags");
+    let mut result = db_tag.find(doc!{"tag": {"$regex": format!("^{}", tag)}}).limit(amount).await?;
+    let mut out = vec![];
+    while let Some(result) = result.next().await{
+        out.push(result.unwrap().tag);
+    }
+    Ok(out)
+}
